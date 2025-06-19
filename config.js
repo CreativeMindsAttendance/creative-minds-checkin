cb3d52dc743c22aa73be16d22e6ea217b1e9dc2b
mxev9 <sasn1000@gmail.com>
2025-06-19 18:23:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-// إحداثيات Creative Minds Institute
+// إحداثيات معهد Creative Minds
 const DEST_LAT = 16.889264;
 const DEST_LON = 42.548691;
+
+// المسافة المسموحة بالكيلومتر
+const allowedDistance = 0.2;
